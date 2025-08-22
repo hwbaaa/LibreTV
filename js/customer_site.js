d2ee136f62a6074596dedcc6523a5049d6255645
hwbaaa <617815706@qq.com>
2025-08-22 17:55:02 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -1,23 +1,7 @@
 const CUSTOMER_SITES = {
     qiqi: {
-        api: 'https://360zy.com/api.php/provide/vod',
-        name: '360资源',
-    }
-    ruyi: {
-        api: 'https://cj.rycjapi.com/api.php/provide/vod',
-        name: '如意资源',
-    }
-        subo: {
-        api: 'https://subocaiji.com/api.php/provide/vod/',
-        name: '速播资源',
-    }
-        guangsu: {
-        api: 'https://api.guangsuapi.com/api.php/provide/vod/',
-        name: '光速资源',
-    }
-        smzy: {
-        api: 'https://img.smdyw.top/api.php/provide/vod',
-        name: '神马资源',
+        api: 'https://www.qiqidys.com/api.php/provide/vod',
+        name: '七七资源',
     }
 };
 
